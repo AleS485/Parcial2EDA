c6e5e22a47c7d4dd730b19822f821833210354b3
Alejandro Peralta <158860526+AleS485@users.noreply.github.com>
2026-06-06 20:35:37 -0300

    guardado prueba jugador

--- Parcial2EDA/src/parcial2eda/Jugador.java
+++ Parcial2EDA/src/parcial2eda/Jugador.java
@@ -95,5 +95,5 @@ public class Jugador {
         return "Jugador{" + "nombreJugador=" + nombreJugador + ", hp=" + hp + ", nivel=" + nivel + ", mochila="
                 + mochila + ", misiones=" + misiones + ", posicion=" + posicion + '}';
     }
-
+    // prueba
 }
